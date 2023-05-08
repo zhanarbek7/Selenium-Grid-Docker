9afb3b34240ab02de7a8b321058567350dd46384
zhanarbekabdurasulov <zhanarbek.a7@gmail.com>
2023-05-07 22:43:52 -0500

    * added 2 plugins to my pom.xml, then if I run mvn test it will execute my testng.xml file as well

--- src/test/java/docker/Setup_DockerGrid.java
+++ src/test/java/docker/Setup_DockerGrid.java
@@ -1,14 +1,18 @@
 package docker;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
 public class Setup_DockerGrid {
 
-    @Test(priority = 1)
-    public void startDockerGrid() {
+    @BeforeTest
+    public void startDockerGrid() throws InterruptedException {
         try {
+            Runtime.getRuntime().exec("sh /Users/zhanarbekabdurasulov/IdeaProjects/QA/selenium-for-docker/start_dockergrid.sh");
+            Thread.sleep(10000);
             String scriptPath = "/Users/zhanarbekabdurasulov/IdeaProjects/QA/selenium-for-docker/start_dockergrid.sh";
             Process process = Runtime.getRuntime().exec(scriptPath);
             int exitCode = process.waitFor();
@@ -17,12 +21,12 @@ public class Setup_DockerGrid {
             } else {
                 System.out.println("Script execution failed");
             }
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException  e) {
             e.printStackTrace();
         }
     }
 
-    @Test(priority = 2)
+    @AfterTest
     public void stopDockerGrid(){
         try {
             String scriptPath = "/Users/zhanarbekabdurasulov/IdeaProjects/QA/selenium-for-docker/stop_dockergrid.sh";
