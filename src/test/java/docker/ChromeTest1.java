c39fb4942729cc90a9bc399451c7f47cbb32e27d
zhanarbekabdurasulov <zhanarbek.a7@gmail.com>
2023-05-07 22:00:13 -0500

    * added some code to see that parallel testing is working

--- src/test/java/docker/ChromeTest1.java
+++ src/test/java/docker/ChromeTest1.java
@@ -7,11 +7,14 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class ChromeTest1 {
 
     @Test
     public void test() throws MalformedURLException {
+        System.out.println(LocalDateTime.now());
         ChromeOptions chromeOptions = new ChromeOptions();
         URL url = new URL("http://localhost:4444/");
         RemoteWebDriver driver = new RemoteWebDriver(url, chromeOptions);
--- src/test/java/docker/FirefoxTest4.java
+++ src/test/java/docker/FirefoxTest4.java
@@ -6,11 +6,13 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.LocalDateTime;
 
 public class FirefoxTest4 {
 
     @Test
     public void test() throws MalformedURLException {
+        System.out.println(LocalDateTime.now());
         FirefoxOptions firefoxOptions = new FirefoxOptions();
         URL url = new URL("http://localhost:4444/");
         RemoteWebDriver driver = new RemoteWebDriver(url, firefoxOptions);
